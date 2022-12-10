57d157756dc8955ced3c25fff460bc91a9572dec
Roman Didukh <newrdlink@yandex.ru>
2022-12-10 21:16:52 +0300

    add gallary working

--- components/eventImagine/index.js
+++ components/eventImagine/index.js
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss'
+import cn from 'classnames'
 import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -10,21 +11,21 @@ const EventImagine = ({ href, image, imageAlt, name, place }) => {
   const [isOver, setIsOver] = useState(false)
 
   return (
-    <li className={styles.eventImagine}
-      onMouseOver={() => setIsOver(true)}
-      onMouseLeave={() => setIsOver(false)}
+    <li className={styles.eventImagine}>
+      <div className={styles.eventImagine__imageWrapper}>
+        <Link href={href}
+          className={cn(styles.eventImagine__link, styles[place])}
+          onMouseOver={() => setIsOver(true)}
+          onMouseLeave={() => setIsOver(false)}
+        />
+        <Image src={image} alt={imageAlt[router.locale]} className={styles.eventImagine__image} priority />
+        <EventName
+          name={name[router.locale]}
+          place={place}
+          isOver={isOver}
+        />
+      </div>
 
-    >
-      <Link href={href}>
-        <div className={styles.eventImagine__imageWrapper}>
-          <Image src={image} alt={imageAlt[router.locale]} className={styles.eventImagine__image} />
-          <EventName
-            name={name[router.locale]}
-            place={place}
-            isOver={isOver}
-          />
-        </div>
-      </Link>
     </li >
   )
 }
--- components/gallery/index.js
+++ components/gallery/index.js
@@ -7,10 +7,13 @@ import useWindowSize from 'helpers/windowsWidth'
 
 const Gallery = () => {
   const container = useRef(null)
+  const containerFront = useRef(null)
   // full width gallery
   const [widthGallery, setWidthGallery] = useState(0)
+  const [widthGalleryFront, setWidthGalleryFront] = useState(0)
   // current left value  
   const [value, setValue] = useState(0)
+  const [valueFront, setValueFront] = useState(0)
   // dynamic move cursor value 
   const [moveValue, setMoveValue] = useState(0)
 
@@ -19,10 +22,12 @@ const Gallery = () => {
   const widthHandle = (evt) => {
     setMoveValue(evt.clientX)
     setValue(moveValue * ((widthGallery - currentUserWidth) / currentUserWidth))
+    setValueFront(moveValue * ((widthGalleryFront - currentUserWidth) / currentUserWidth))
   }
 
   useEffect(() => {
     setWidthGallery(container.current.scrollWidth)
+    setWidthGalleryFront(containerFront.current.scrollWidth)
   }, [])
 
   return (
@@ -30,10 +35,24 @@ const Gallery = () => {
       <ul className={styles.gallery__list}
         ref={container}
         onMouseMove={widthHandle}
-        style={{ left: `-${value}px` }}
-      >
+        style={{ left: `-${value}px` }}>
         {
-          items.map((el) => <EventImagine
+          items.slice(7).map((el) => <EventImagine
+            key={el.id}
+            href={el.path}
+            imageAlt={el.imageAlt}
+            name={el.name}
+            image={el.image}
+            place={el.place}
+          />)
+        }
+      </ul>
+      <ul className={styles.gallery__list_front}
+        ref={containerFront}
+        onMouseMove={widthHandle}
+        style={{ left: `-${valueFront}px` }}>
+        {
+          items.slice(0, 7).map((el) => <EventImagine
             key={el.id}
             href={el.path}
             imageAlt={el.imageAlt}
@@ -45,6 +64,7 @@ const Gallery = () => {
       </ul>
       <p>{useWindowSize()}</p>
       <p>{widthGallery}</p>
+      <p>{widthGalleryFront}</p>
     </div>
 
   )
