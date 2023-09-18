fc19471de5689fb49ce32d98ad7f25aabbb8bf3a
newrdlink <newrdlink@gmail.com>
2023-09-18 20:37:28 +0300

    add some things performance page

--- pages/performances/[id].js
+++ pages/performances/[id].js
@@ -1,48 +1,76 @@
-import styles from './styles.module.scss'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import apiPerformance from 'utils/ApiPerformance'
-import { myCarousel, Line, errors, navigations } from 'components'
+import styles from "./styles.module.scss";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import apiPerformance from "utils/ApiPerformance";
+import { myCarousel, Line, errors, navigations } from "components";
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { API_URL } from "configs/variables";
 
 const PerformancePage = ({ data }) => {
-  const router = useRouter()
-  // console.log(data)
+  const router = useRouter();
+  console.log(data);
 
   useEffect(() => {
     // console.log(1)
-  }, [])
+  }, []);
 
-  if (data === null) return <errors.Performance />
-  const { attributes: { images: { data: arrImg } } } = data
+  if (data === null) return <errors.Performance />;
+  const {
+    attributes: {
+      images: { data: arrImg },
+      introduce,
+    },
+  } = data;
 
-  const firstData = data.attributes
-  const secondData = data.attributes.localizations.data[0].attributes
-  const currentData = data.reqLocation === firstData.locale ? firstData : secondData
+  const firstData = data.attributes;
+  const secondData = data.attributes.localizations.data[0].attributes;
+  const currentData =
+    data.reqLocation === firstData.locale ? firstData : secondData;
 
   return (
     <>
       <Line />
       <div className={styles.performancePage}>
         <navigations.PerformancePage name={currentData.name} />
+        <section className={styles.performancePage__promo}>
+          <div className={styles.performancePage__promoContent}>
+            <div>
+              <ReactMarkdown className={styles.performancePage__introduce}>
+                {currentData.introduce}
+              </ReactMarkdown>
+            </div>
+            <div className={styles.performancePage__imgWrapper}>
+              <Image
+                className={styles.performancePage__img}
+                alt="#"
+                src={API_URL + (firstData.image.data?.attributes.url || "")}
+                width={firstData.image.data?.attributes.width || 100}
+                height={firstData.image.data?.attributes.height || 100}
+              />
+            </div>
+          </div>
+        </section>
 
         {/* <myCarousel.React arrImg={arrImg} /> */}
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export async function getServerSideProps(context) {
-  const { params: { id }, locale } = context
-  const res = await apiPerformance.getPerformance(id)
+  const {
+    params: { id },
+    locale,
+  } = context;
+  const res = await apiPerformance.getPerformance(id);
 
   if (!!res.data) {
-    res.data.reqLocation = locale
-    return { props: { data: res.data } }
+    res.data.reqLocation = locale;
+    return { props: { data: res.data } };
   }
 
-  return { props: { data: res.data } }
+  return { props: { data: res.data } };
 }
 
-
-export default PerformancePage
+export default PerformancePage;
